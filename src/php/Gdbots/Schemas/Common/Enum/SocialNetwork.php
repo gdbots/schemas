c842f1e2fd75c1939f0269ae6fb1b281fee6f529
Michael H. Arieli <michaelharieli@gmail.com>
2017-02-14 14:16:26 -0800

    Added "social-network" enum (#12)
    * Added "social-network" enum
    
    * Added additional social networks

---
+++ src/php/Gdbots/Schemas/Common/Enum/SocialNetwork.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Gdbots\Schemas\Common\Enum;
+
+use Gdbots\Common\Enum;
+
+/**
+ * @method static SocialNetwork UNKNOWN()
+ * @method static SocialNetwork FACEBOOK()
+ * @method static SocialNetwork QQ()
+ * @method static SocialNetwork WHATSAPP()
+ * @method static SocialNetwork QZONE()
+ * @method static SocialNetwork WECHAT()
+ * @method static SocialNetwork LINKEDIN()
+ * @method static SocialNetwork SKYPE()
+ * @method static SocialNetwork GOOGLEPLUS()
+ * @method static SocialNetwork INSTAGRAM()
+ * @method static SocialNetwork BAIDUTIEBA()
+ * @method static SocialNetwork TWITTER()
+ * @method static SocialNetwork VIBER()
+ * @method static SocialNetwork TUMBLR()
+ * @method static SocialNetwork SNAPCHAT()
+ * @method static SocialNetwork LINE()
+ * @method static SocialNetwork SINAWEIBO()
+ * @method static SocialNetwork VK()
+ * @method static SocialNetwork REDDIT()
+ * @method static SocialNetwork YY()
+ * @method static SocialNetwork TARINGA()
+ * @method static SocialNetwork TELEGRAM()
+ * @method static SocialNetwork TAGGED()
+ * @method static SocialNetwork MYSPACE()
+ * @method static SocialNetwork BADOO()
+ * @method static SocialNetwork STUMBLEUPON()
+ * @method static SocialNetwork FOURSQUARE()
+ * @method static SocialNetwork MEETME()
+ * @method static SocialNetwork MEETUP()
+ * @method static SocialNetwork SKYROCK()
+ * @method static SocialNetwork DELICIOUS()
+ * @method static SocialNetwork KIWIBOX()
+ * @method static SocialNetwork SNAPFISH()
+ * @method static SocialNetwork FLICKR()
+ * @method static SocialNetwork PHOTOBUCKET()
+ * @method static SocialNetwork DEVIANTART()
+ * @method static SocialNetwork FOTOLOG()
+ * @method static SocialNetwork PINTEREST()
+ * @method static SocialNetwork BUZZNET()
+ * @method static SocialNetwork WEHEARTIT()
+ * @method static SocialNetwork PATH()
+ * @method static SocialNetwork LASTFM()
+ * @method static SocialNetwork REVERBNATION()
+ * @method static SocialNetwork CROSSTV()
+ * @method static SocialNetwork FLIXSTER()
+ * @method static SocialNetwork GAIAONLINE()
+ * @method static SocialNetwork BLACKPLANET()
+ * @method static SocialNetwork CARE2()
+ * @method static SocialNetwork CARINGBRIDGE()
+ * @method static SocialNetwork VAMPIREFREAKS()
+ * @method static SocialNetwork CAFEMOM()
+ * @method static SocialNetwork RAVELRY()
+ * @method static SocialNetwork ASMALLWORLD()
+ * @method static SocialNetwork NEXTDOOR()
+ * @method static SocialNetwork WAYN()
+ * @method static SocialNetwork COUCHSURFING()
+ * @method static SocialNetwork TRAVBUDDY()
+ * @method static SocialNetwork CELLUFUN()
+ * @method static SocialNetwork MOCOSPACE()
+ * @method static SocialNetwork ITSMY()
+ * @method static SocialNetwork YOUTUBE()
+ * @method static SocialNetwork YOUKU()
+ * @method static SocialNetwork FUNNYORDIE()
+ * @method static SocialNetwork TOUT()
+ * @method static SocialNetwork VINE()
+ * @method static SocialNetwork CLASSMATES()
+ * @method static SocialNetwork MYLIFE()
+ * @method static SocialNetwork MYHERITAGE()
+ * @method static SocialNetwork VIADEO()
+ * @method static SocialNetwork RYZE()
+ * @method static SocialNetwork XING()
+ * @method static SocialNetwork WEEWORLD()
+ * @method static SocialNetwork HABBO()
+ * @method static SocialNetwork TUENTI()
+ * @method static SocialNetwork XANGA()
+ * @method static SocialNetwork SOLABORATE()
+ * @method static SocialNetwork PLURK()
+ * @method static SocialNetwork LIVEJOURNAL()
+ */
+final class SocialNetwork extends Enum
+{
+    const UNKNOWN = 'unknown';
+    const FACEBOOK = 'facebook';
+    const QQ = 'qq';
+    const WHATSAPP = 'whatsapp';
+    const QZONE = 'qzone';
+    const WECHAT = 'wechat';
+    const LINKEDIN = 'linkedin';
+    const SKYPE = 'skype';
+    const GOOGLEPLUS = 'googleplus';
+    const INSTAGRAM = 'instagram';
+    const BAIDUTIEBA = 'baidutieba';
+    const TWITTER = 'twitter';
+    const VIBER = 'viber';
+    const TUMBLR = 'tumblr';
+    const SNAPCHAT = 'snapchat';
+    const LINE = 'line';
+    const SINAWEIBO = 'sinaweibo';
+    const VK = 'vk';
+    const REDDIT = 'reddit';
+    const YY = 'yy';
+    const TARINGA = 'taringa';
+    const TELEGRAM = 'telegram';
+    const TAGGED = 'tagged';
+    const MYSPACE = 'myspace';
+    const BADOO = 'badoo';
+    const STUMBLEUPON = 'stumbleupon';
+    const FOURSQUARE = 'foursquare';
+    const MEETME = 'meetme';
+    const MEETUP = 'meetup';
+    const SKYROCK = 'skyrock';
+    const DELICIOUS = 'delicious';
+    const KIWIBOX = 'kiwibox';
+    const SNAPFISH = 'snapfish';
+    const FLICKR = 'flickr';
+    const PHOTOBUCKET = 'photobucket';
+    const DEVIANTART = 'deviantart';
+    const FOTOLOG = 'fotolog';
+    const PINTEREST = 'pinterest';
+    const BUZZNET = 'buzznet';
+    const WEHEARTIT = 'weheartit';
+    const PATH = 'path';
+    const LASTFM = 'lastfm';
+    const REVERBNATION = 'reverbnation';
+    const CROSSTV = 'crosstv';
+    const FLIXSTER = 'flixster';
+    const GAIAONLINE = 'gaiaonline';
+    const BLACKPLANET = 'blackplanet';
+    const CARE2 = 'care2';
+    const CARINGBRIDGE = 'caringbridge';
+    const VAMPIREFREAKS = 'vampirefreaks';
+    const CAFEMOM = 'cafemom';
+    const RAVELRY = 'ravelry';
+    const ASMALLWORLD = 'asmallworld';
+    const NEXTDOOR = 'nextdoor';
+    const WAYN = 'wayn';
+    const COUCHSURFING = 'couchsurfing';
+    const TRAVBUDDY = 'travbuddy';
+    const CELLUFUN = 'cellufun';
+    const MOCOSPACE = 'mocospace';
+    const ITSMY = 'itsmy';
+    const YOUTUBE = 'youtube';
+    const YOUKU = 'youku';
+    const FUNNYORDIE = 'funnyordie';
+    const TOUT = 'tout';
+    const VINE = 'vine';
+    const CLASSMATES = 'classmates';
+    const MYLIFE = 'mylife';
+    const MYHERITAGE = 'myheritage';
+    const VIADEO = 'viadeo';
+    const RYZE = 'ryze';
+    const XING = 'xing';
+    const WEEWORLD = 'weeworld';
+    const HABBO = 'habbo';
+    const TUENTI = 'tuenti';
+    const XANGA = 'xanga';
+    const SOLABORATE = 'solaborate';
+    const PLURK = 'plurk';
+    const LIVEJOURNAL = 'livejournal';
+}
