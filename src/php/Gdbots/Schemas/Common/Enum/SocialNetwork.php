a4e653b8056f62b089fa868eb96166cd96fc1193
Michael H. Arieli <michaelharieli@gmail.com>
2017-02-14 11:58:41 -0800

    Added "social-network" enum

---
+++ src/php/Gdbots/Schemas/Common/Enum/SocialNetwork.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Gdbots\Schemas\Common\Enum;
+
+use Gdbots\Common\Enum;
+
+/**
+ * @method static SocialNetwork UNKNOWN()
+ * @method static SocialNetwork FACEBOOK()
+ * @method static SocialNetwork FLICKR()
+ * @method static SocialNetwork GOOGLEPLUS()
+ * @method static SocialNetwork INSTAGRAM()
+ * @method static SocialNetwork LINKEDIN()
+ * @method static SocialNetwork MYSPACE()
+ * @method static SocialNetwork PINTEREST()
+ * @method static SocialNetwork REDDIT()
+ * @method static SocialNetwork SNAPCHAT()
+ * @method static SocialNetwork TUMBLR()
+ * @method static SocialNetwork TWITTER()
+ * @method static SocialNetwork VINE()
+ * @method static SocialNetwork YOUTUBE()
+ */
+final class SocialNetwork extends Enum
+{
+    const UNKNOWN = 'unknown';
+    const FACEBOOK = 'facebook';
+    const FLICKR = 'flickr';
+    const GOOGLEPLUS = 'googleplus';
+    const INSTAGRAM = 'instagram';
+    const LINKEDIN = 'linkedin';
+    const MYSPACE = 'myspace';
+    const PINTEREST = 'pinterest';
+    const REDDIT = 'reddit';
+    const SNAPCHAT = 'snapchat';
+    const TUMBLR = 'tumblr';
+    const TWITTER = 'twitter';
+    const VINE = 'vine';
+    const YOUTUBE = 'youtube';
+}
