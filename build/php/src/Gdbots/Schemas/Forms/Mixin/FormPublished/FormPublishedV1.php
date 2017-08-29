1a609e55280a019e59fa42fca33d318b3aa720c4
bhsiao55 <brian.hsiao@wbdevops.com>
2017-08-29 15:04:10 -0700

    add form-published, publish-form schema

---
+++ build/php/src/Gdbots/Schemas/Forms/Mixin/FormPublished/FormPublishedV1.php
@@ -0,0 +1,7 @@
+<?php
+// @link http://schemas.gdbots.io/json-schema/gdbots/forms/mixin/form-published/1-0-0.json#
+namespace Gdbots\Schemas\Forms\Mixin\FormPublished;
+
+interface FormPublishedV1 extends FormPublished
+{
+}
---
+++ build/php/src/Gdbots/Schemas/Forms/Mixin/PublishForm/PublishFormV1.php
@@ -0,0 +1,7 @@
+<?php
+// @link http://schemas.gdbots.io/json-schema/gdbots/forms/mixin/publish-form/1-0-0.json#
+namespace Gdbots\Schemas\Forms\Mixin\PublishForm;
+
+interface PublishFormV1 extends PublishForm
+{
+}
