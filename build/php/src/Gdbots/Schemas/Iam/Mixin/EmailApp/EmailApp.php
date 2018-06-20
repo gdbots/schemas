2fd79c446ca2b40ef91907be6fa07a2b877f71b1
Greg Brown <greg@12dnetworks.com>
2018-06-20 13:42:48 -0700

    add gdbots:iam:mixin:email-app

---
+++ build/php/src/Gdbots/Schemas/Iam/Mixin/EmailApp/EmailApp.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.gdbots.io/json-schema/gdbots/iam/mixin/email-app/latest.json#
+namespace Gdbots\Schemas\Iam\Mixin\EmailApp;
+
+use Gdbots\Schemas\Iam\Mixin\App\App;
+
+interface EmailApp extends App
+{
+}
---
+++ build/php/src/Gdbots/Schemas/Iam/Mixin/EmailApp/EmailAppV1Trait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gdbots\Schemas\Iam\Mixin\EmailApp;
+
+use Gdbots\Pbj\Schema;
+
+/**
+ * @method static Schema schema
+ * @method mixed get($fieldName, $default = null)
+ */
+trait EmailAppV1Trait
+{
+    /**
+     * @return array
+     */
+    public function getUriTemplateVars()
+    {
+        return [
+            '_id' => (string)$this->get('_id'),
+        ];
+    }
+}
